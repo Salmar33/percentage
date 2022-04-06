b8070ee4ab706187528e9ac3e73b27b6a8083e27
Thomas Schlögl <thomas@schloegl.wien>
2022-04-07 00:02:34 +0200

    .) added custom changes as per my old version

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -7,10 +7,12 @@ namespace percentage
 {
     class TrayIcon
     {
+        private const int UPDATE_RATE = 5000;  //update rate in milliseconds
+
         [DllImport("user32.dll", CharSet=CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
 
-        private const int fontSize = 18;
+        private const int fontSize = 22;
         private const string font = "Segoe UI";
 
         private NotifyIcon notifyIcon;
@@ -32,7 +34,7 @@ namespace percentage
             notifyIcon.Visible = true;
 
             Timer timer = new Timer();
-            timer.Interval = 1000;
+            timer.Interval = UPDATE_RATE;
             timer.Tick += new EventHandler(TimerTick);
             timer.Start();
         }
@@ -40,13 +42,17 @@ namespace percentage
         private Bitmap GetTextBitmap(String text, Font font, Color fontColor)
         {
             SizeF imageSize = GetStringImageSize(text, font);
+
+            // added by me to manipulate the size
+            imageSize.Width = 32; imageSize.Height = 32;
+
             Bitmap bitmap = new Bitmap((int)imageSize.Width, (int)imageSize.Height);
             using (Graphics graphics = Graphics.FromImage(bitmap))
             {
                 graphics.Clear(Color.FromArgb(0, 0, 0, 0));
                 using (Brush brush = new SolidBrush(fontColor))
                 {
-                    graphics.DrawString(text, font, brush, 0, 0);
+                    graphics.DrawString(text, font, brush, -6, -5);
                     graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
                     graphics.Save();
                 }
@@ -73,8 +79,35 @@ namespace percentage
             PowerStatus powerStatus = SystemInformation.PowerStatus;
             String percentage = (powerStatus.BatteryLifePercent * 100).ToString();
             bool isCharging = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Online;
-            String bitmapText = isCharging ? percentage + "*" : percentage;
-            using (Bitmap bitmap = new Bitmap(GetTextBitmap(bitmapText, new Font(font, fontSize), Color.White)))
+            //String bitmapText = isCharging ? percentage + "*" : percentage;
+            String bitmapText = percentage;
+            Color textColor;
+
+
+            // DEBUG
+            bitmapText = "99";
+            textColor = Color.Orange;
+            // END DEBUG
+
+
+            // if(powerStatus.BatteryLifePercent >= 0.4)
+            // {
+            //     textColor = Color.Lime;
+            // }
+            // else if(powerStatus.BatteryLifePercent < 0.4 && powerStatus.BatteryLifePercent >= 0.3)
+            // {
+            //     textColor = Color.Yellow;
+            // }
+            // else if(powerStatus.BatteryLifePercent < 0.3 && powerStatus.BatteryLifePercent >= 0.2)
+            // {
+            //     textColor = Color.Orange;
+            // }
+            // else
+            // {
+            //     textColor = Color.Red;
+            // }
+
+            using (Bitmap bitmap = new Bitmap(GetTextBitmap(bitmapText, new Font(font, fontSize, FontStyle.Bold), textColor)))
             {
                 System.IntPtr intPtr = bitmap.GetHicon();
                 try
